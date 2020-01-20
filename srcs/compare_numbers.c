c59cc3e0dd753ebefe32bc88606788e41ef66216
Salah Adawi <53702890+Hahli144@users.noreply.github.com>
2020-01-20 14:20:38 +0200

    Move functions to b_stack_lookahead and rename

--- srcs/compare_numbers.c
+++ srcs/compare_numbers.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   compare_numbers.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -14,77 +14,6 @@
 #include "libft/ft_printf.h"
 #include "libft/get_next_line.h"
 #include "checker.h"
-
-
-
-int		check_number_group(int n, int **ab, int group_amount)
-{
-	int		i;
-	int		j;
-	float	divisor;
-
-	i = 0;
-	j = 0;
-	divisor = ((float)*ab[2] + (float)*ab[3]) / (float)group_amount;
-	while (*ab[2] > i)
-	{
-		if (ab[0][i] < n)
-			j++;
-		i++;
-	}
-	i = 0;
-	while (*ab[3] > i)
-	{
-		if (ab[1][i] < n)
-			j++;
-		i++;
-	}
-	return (j / divisor);
-}
-
-
-
-
-int		check_if_b_moves(int **ab)
-{
-	int biggest;
-	int i;
-
-	i = 0;
-	biggest = 0;
-	if (!smaller_than_stack(ab[0][0], ab, 1)
-		&& !bigger_than_stack(ab[0][0], ab, 1))
-		return (0);
-	while (*ab[3] > ++i)
-	{
-		if (ab[1][i] > ab[1][biggest])
-			biggest = i;
-	}
-	if (biggest <= *ab[3] / 2)
-		return (biggest);
-	else
-		return ((*ab[3] - biggest) * -1);
-}
-
-
-
-
-
-int		stack_contains_group(int **ab, int stack, int group, int group_amount)
-{
-	int i;
-
-	i = 0;
-	if (!*ab[stack + 2])
-		return (0);
-	while (*ab[stack + 2] > i)
-	{
-		if (check_number_group(ab[stack][i], ab, group_amount) == group)
-			return (1);
-		i++;
-	}
-	return (0);
-}
 
 int		smaller_than_stack(int n, int **ab, int stack)
 {
@@ -106,16 +35,4 @@ int		bigger_than_stack(int n, int **ab, int stack)
 		if (n < ab[stack][i++])
 			return (0);
 	return (1);
-}
-
-
-
-
-
-int		count_group_amount(int **ab)
-{
-	if (*ab[2] < 300)
-		return (5);
-	else
-		return (11);
 }
