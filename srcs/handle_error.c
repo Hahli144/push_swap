fad5994a2ebadcd40be6527c969a18a78d3b6fce
Salah Adawi <53702890+Hahli144@users.noreply.github.com>
2020-01-20 14:03:52 +0200

    Create handle_error.c

---
+++ srcs/handle_error.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_error.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/08 15:58:18 by sadawi            #+#    #+#             */
+/*   Updated: 2020/01/17 16:52:44 by sadawi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+#include "libft/ft_printf.h"
+#include "libft/get_next_line.h"
+#include "checker.h"
+
+int		handle_error(void)
+{
+	write(2, "Error\n", 6);
+	return (1);
+}
+
+int		check_error(int argc, char **argv)
+{
+	if (argc < 2)
+		return (1);
+	if (!check_array_int(argc, argv))
+		return (1);
+	return (0);
+}
