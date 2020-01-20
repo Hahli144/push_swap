e0cbd46b0fa6fad8852991ddcf7d91996522ec0c
Salah Adawi <53702890+Hahli144@users.noreply.github.com>
2020-01-20 13:57:54 +0200

    Move functions to sort_stack.c

--- srcs/functions2.c
+++ srcs/functions2.c
@@ -252,101 +252,10 @@ void	debug_print_wait(int **ab)
 	read(0, NULL, 1);
 }
 
-void	sort_stack_small(int **ab, int debug_mode)
-{
-	int i;
-
-	i = 0;
-	if (check_order(ab))
-		return ;
-	if (debug_mode)
-		debug_print_wait(ab);
-	while (!bigger_than_stack(ab[0][i], ab, 0))
-		i++;
-	if (i == 0)
-		operation_print(ab, "ra", debug_mode);
-	if (i == 1)
-		operation_print(ab, "rra", debug_mode);
-	if (!check_order_stack(ab, 0))
-		operation_print(ab, "sa", debug_mode);
-}
-
-void	sort_stack_medium(int **ab, int debug_mode)
-{
-	int i;
-
-	i = 0;
-	if (check_order(ab))
-		return ;
-	if (debug_mode)
-		debug_print_wait(ab);
-	biggest_to_top(ab, 0);
-	if (debug_mode)
-		debug_print_wait(ab);
-	operation_print(ab, "pb", debug_mode);
-	smallest_to_top(ab, 0);
-	if (debug_mode)
-		debug_print_wait(ab);
-	operation_print(ab, "pb", debug_mode);
-	sort_stack_small(ab, debug_mode);
-	operation_print(ab, "pa", debug_mode);
-	operation_print(ab, "pa", debug_mode);
-	operation_print(ab, "ra", debug_mode);
-}
-
 int		count_group_amount(int **ab)
 {
 	if (*ab[2] < 300)
 		return (5);
 	else
 		return (11);
-}
-
-void	sort_stack_groups(int **ab, int debug_mode)
-{
-	int group;
-	int group_amount;
-
-	if (check_order(ab))
-		return ;
-	group = 0;
-	group_amount = count_group_amount(ab);
-	if (debug_mode)
-		debug_print_wait(ab);
-	while (*ab[2])
-	{
-		while (stack_contains_group(ab, 0, group, group_amount))
-		{
-			group_number_to_top(ab, group, group_amount);
-			rotate_stack_b(ab);
-			operation_print(ab, "pb", debug_mode);
-		}
-		group++;
-	}
-	while (*ab[3])
-	{
-		biggest_to_top(ab, 1);
-		operation_print(ab, "pa", debug_mode);
-	}
-}
-
-int		main(int argc, char **argv)
-{
-	int *ab[4];
-
-	if (argc == 1 || (argc == 2 && ft_strequ(argv[1], "-v")))
-		return (0);
-	if (check_error(argc, argv))
-		return (handle_error());
-	if (create_stacks(argc, argv, ab))
-		return (1);
-	if (check_duplicate(ab))
-		return (handle_error());
-	if (*ab[2] < 4)
-		sort_stack_small(ab, ft_strequ(argv[1], "-v"));
-	else if (*ab[2] < 6)
-		sort_stack_medium(ab, ft_strequ(argv[1], "-v"));
-	else
-		sort_stack_groups(ab, ft_strequ(argv[1], "-v"));
-	return (0);
 }
